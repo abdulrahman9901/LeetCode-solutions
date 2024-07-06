09c6a554f597eb7be518ded2d13a4f0cff6e019b
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2024-07-06 12:41:21 +0300

    [2839] [Time Beats: 12.50%] [Memory Beats: 12.50%] - LeetPush

---
+++ Automatically Added/2839-Check-if-Strings-Can-be-Made-Equal-With-Operations-I.c
@@ -0,0 +1,17 @@
+char tmp;
+#define SWAP(a,b)  tmp=a,a=b,b=tmp
+
+bool canBeEqual(char* s1, char* s2){
+    if(strcmp(s1,s2) == 0)
+        return true;
+    SWAP(s1[0],s1[2]);
+    if(strcmp(s1,s2) == 0)
+        return true;
+    SWAP(s1[1],s1[3]);
+    if(strcmp(s1,s2) == 0)
+        return true;
+    SWAP(s1[0],s1[2]);
+    if(strcmp(s1,s2) == 0)
+        return true;
+    return false;
+}
