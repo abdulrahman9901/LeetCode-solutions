1693265f6cd6f7361819b9ee7c5947b6493ab12c
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2024-07-06 11:40:54 +0300

    [846] [Time Beats: 7.92%] [Memory Beats: 5.10%] - LeetPush

---
+++ Automatically Added/846-Hand-of-Straights.c
@@ -0,0 +1,34 @@
+int compare (const void * a ,const void * b){
+    return *((int*)a) - *((int*)b);
+}
+
+bool isNStraightHand(int* hand, int handSize, int groupSize) {
+    qsort(hand,handSize,sizeof(int),compare);
+    int i = 0 , prev;
+    if(handSize % groupSize != 0 )
+            return false;
+
+    while(i<handSize){
+        if(i%groupSize == 0){
+            prev = hand[i];
+        }else{
+            int x = i;
+            while(x <= handSize){
+                if(x >= handSize)
+                    return false;
+                if(prev + 1 == hand[x]){
+                    prev = hand[x];
+                    hand[x] = hand[i];
+                    hand[i] = prev;
+                    if(x + 1 < handSize && i != x ){
+                        qsort(hand+i+1,(x-i+1),sizeof(int),compare);
+                    }
+                    break;
+                }
+                x++;
+            }
+        }
+        i++;
+    }
+    return true;
+}
