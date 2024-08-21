eda48a96be91c1bdd25f94f0cc597c9a069ae899
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2024-08-21 09:07:02 +0300

    [190] [Time Beats: 10.18%] [Memory Beats: 5.21%] - LeetPush

--- Automatically Added/190-Reverse-Bits.cpp
+++ Automatically Added/190-Reverse-Bits.cpp
@@ -1,19 +1,16 @@
 class Solution {
-    string to_bin(uint32_t n){
-        string bin = "";
-        while(n){
-            if(n%2 == 1)
-                bin+="1";
-            else
-                bin+="0";
-            n /=2;
-        }
-        while(bin.size() < 32)
-            bin+="0";
-        return bin;
-    }
 public:
     uint32_t reverseBits(uint32_t n) {
-        return stoll(to_bin(n), nullptr, 2);
+        map<int,pair<int,int>> masks = {
+            {16,{0xffff0000,0x0000ffff}},
+            {8,{0xff00ff00,0x00ff00ff}},
+            {4,{0xf0f0f0f0,0x0f0f0f0f}},
+            {2,{0xcccccccc,0x33333333}},
+            {1,{0xaaaaaaaa,0x55555555}},        
+        };
+        for(auto mask:masks)
+            n = (n&mask.second.first) >> mask.first | (n&mask.second.second) << mask.first;
+
+        return n;
     }
 };
