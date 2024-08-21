77c8e9384e8bd91878bd25a5d0c506cca67fea8a
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2024-08-21 08:50:16 +0300

    Time: 0 ms (100%), Space: 9.3 MB (66.91%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,9 @@
 class Solution {
-    int count_ones(int n){
-        int c = 0;
-        while(n){
-            n &=(n-1);
-            c++;
-        }
-        return c;
-    }
 public:
     vector<int> countBits(int n) {
         vector<int> res(n+1,0);
-        for(int j=0;j<n+1;j++){
-            res[j] = count_ones(j);
+        for(int j=1;j<=n;j++){
+            res[j] = res[j/2] + j%2;
         }
         return res;
     }
