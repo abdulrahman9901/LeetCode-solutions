edb94da401b570fb578b7445f870836b2ece67a4
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2024-08-21 08:22:59 +0300

    [268] [Time Beats: 50.62%] [Memory Beats: 61.75%] - LeetPush

--- Automatically Added/268-Missing-Number.cpp
+++ Automatically Added/268-Missing-Number.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i ;
-        for(i = 1 ; i <nums.size();++i){
-            if(nums[i] != nums[i-1]+1)
-                return nums[i-1]+1;
+        int res=0;
+        for(int i = 0 ; i <nums.size();++i){
+            res ^=i^nums[i]; 
         }
-        return i == nums.size() && nums[i-1] !=nums.size()? nums.size() : 0;
+        return res^nums.size();
     }
 };
