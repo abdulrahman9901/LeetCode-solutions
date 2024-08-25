eba4386a776b1d7c36fdc4608c9f67ea597247f1
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2024-08-25 18:51:06 +0300

    Time: 378 ms (5.24%), Space: 73.1 MB (5.05%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> maxEle(nums.size()+1,0);
+        if(nums.size() == 0)
+            return 0;
+        if(nums.size() == 1)
+            return nums[0];
+
+        maxEle[0] = nums[0];
+        int maxSum = maxEle[0];
+        for(int i=1;i<nums.size();++i){
+            maxEle[i] = max(nums[i],maxEle[i-1]+nums[i]);
+            maxSum = max(maxSum,maxEle[i]);
+            cout<<maxEle[i]<<" "<<maxSum<<endl;
+        }
+
+        return maxSum;
+    }
+};
